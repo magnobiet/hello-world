118848917abb82a961bb2fe1476323d5fc306287
Magno Biét <magno.biet@gmail.com>
2016-09-30 16:55:04 -0300

    Add C# sample. Closes issue #23

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,23 @@
+/*!
+ * A Hello World! program in C#.
+ */
+
+using System;
+
+namespace HelloWorld
+{
+
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!\n");
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
