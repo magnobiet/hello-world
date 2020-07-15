20343f5cbbbc3ee77dc7dc40e372319cc73f23a2
Magno F. B. Biét <magnobiet@users.noreply.github.com>
2020-07-15 08:42:22 -0300

    Update hello-world.cob

--- COBOL/hello-world.cob
+++ COBOL/hello-world.cob
@@ -1,6 +1,6 @@
-           * Hello World! in COBOL
-            IDENTIFICATION DIVISION.
-            PROGRAM-ID. hello-world.
-            PROCEDURE DIVISION.
-            	DISPLAY "Hello World!".
-            STOP RUN.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. hello-world.
+
+PROCEDURE DIVISION.
+    DISPLAY 'Hello world!'.
+STOP RUN.
